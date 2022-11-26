739fead50457627405b7b5c7a228603a893736ac
Beni <benik@live.ca>
2022-11-25 21:10:28 -0500

    remove fake secret key

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o_d4h5i=du+k3-*mr8p*gh1dr2g)#8x7jg2v_e6($e6y^gjb3+"
 
 
 SECRET_KEY = config('SECRET_KEY')
